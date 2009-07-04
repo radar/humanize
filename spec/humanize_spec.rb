5bc90b7664181f5250808f57018255f041d9c531
Ryan Bigg <radarlistener@gmail.com>
2009-07-04 18:41:00 +1000

    Tests are now much shorter thanks to Mitchell Riley's Emacs proskills

--- spec/humanize_spec.rb
+++ spec/humanize_spec.rb
@@ -15,83 +15,30 @@ describe "Humanize" do
     [11, "eleven"],
     [21, "twenty one"],
     [100, "one hundred"],
+    [100, "one hundred"],
+    [101, "one hundred and one"],
+    [111, "one hundred and eleven"],
+    [121, "one hundred and twenty one"],
+    [1079, "one thousand seventy nine"],
+    [10079, "ten thousand seventy nine"],
+    [235409, "two hundred and thirty five thousand four hundred and nine"],
+    [1007310, "one million, seven thousand three hundred and ten"],
+    [21007310, "twenty one million, seven thousand three hundred and ten"],
+    [321007310, "three hundred and twenty one million, seven thousand three hundred and ten"],
+    [4321007310, "four billion, three hundred and twenty one million, seven thousand three hundred and ten"],
+    [14321007310, "fourteen billion, three hundred and twenty one million, seven thousand three hundred and ten"],
+    [514321007310, "five hundred and fourteen billion, three hundred and twenty one million, seven thousand three hundred and ten"],
+    [6514321007310, "six trillion, five hundred and fourteen billion, three hundred and twenty one million, seven thousand three hundred and ten"],
+    [76514321007310, "seventy six trillion, five hundred and fourteen billion, three hundred and twenty one million, seven thousand three hundred and ten"],
+    [176514321007310, "one hundred and seventy six trillion, five hundred and fourteen billion, three hundred and twenty one million, seven thousand three hundred and ten"],
+    [2176514321007310, "two quadrillion, one hundred and seventy six trillion, five hundred and fourteen billion, three hundred and twenty one million, seven thousand three hundred and ten"],
+    [12176514321007310, "twelve quadrillion, one hundred and seventy six trillion, five hundred and fourteen billion, three hundred and twenty one million, seven thousand three hundred and ten"]
   ].each do |num, human|
     it "#{num} is #{human}" do
       num.humanize.should eql(human)
     end
   end
-  
-  it "100 is one hundred" do
-    100.humanize.should eql("one hundred")
-  end
-  
-  it "101 is one hundred and one" do
-    101.humanize.should eql("one hundred and one")
-  end
-  
-  it "111 is one hundred and eleven" do
-    111.humanize.should eql("one hundred and eleven")
-  end
-  
-  it "121 is one hundred and twenty one" do
-    121.humanize.should eql("one hundred and twenty one")
-  end
-  
-  it "1079 is one thousand and seventy nine" do
-    1079.humanize.should eql("one thousand seventy nine")
-  end
-  
-  it "10079 is one thousand and seventy nine" do
-    10079.humanize.should eql("ten thousand seventy nine")
-  end
-  
-  it "235409 is two hundred and thirty five thousand four hundred and nine" do
-    235409.humanize.should eql("two hundred and thirty five thousand four hundred and nine")
-  end
-  
-  it "1007310 is one million seven thousand three hundred and ten" do
-    1007310.humanize.should eql("one million, seven thousand three hundred and ten")
-  end
-  
-  it "21007310 is twenty one million seven thousand three hundred and ten" do
-    21007310.humanize.should eql("twenty one million, seven thousand three hundred and ten")
-  end
-  
-  it "321007310 is three hundred and twenty one million seven thousand three hundred and ten" do
-    321007310.humanize.should eql("three hundred and twenty one million, seven thousand three hundred and ten")
-  end
-  
-  it "4321007310 is four billion, three hundred and twenty one million seven thousand three hundred and ten" do
-    4321007310.humanize.should eql("four billion, three hundred and twenty one million, seven thousand three hundred and ten")
-  end
-  
-  it "14321007310 is fourteen billion, three hundred and twenty one million seven thousand three hundred and ten" do
-    14321007310.humanize.should eql("fourteen billion, three hundred and twenty one million, seven thousand three hundred and ten")
-  end
-  
-  it "514321007310 is five hundred and fourteen billion, three hundred and twenty one million seven thousand three hundred and ten" do
-    514321007310.humanize.should eql("five hundred and fourteen billion, three hundred and twenty one million, seven thousand three hundred and ten")
-  end
-  
-  it "6514321007310 is six trillion five hundred and fourteen billion, three hundred and twenty one million seven thousand three hundred and ten" do
-    6514321007310.humanize.should eql("six trillion, five hundred and fourteen billion, three hundred and twenty one million, seven thousand three hundred and ten")
-  end
-  
-  it "76514321007310 is seventy six trillion five hundred and fourteen billion, three hundred and twenty one million seven thousand three hundred and ten" do
-    76514321007310.humanize.should eql("seventy six trillion, five hundred and fourteen billion, three hundred and twenty one million, seven thousand three hundred and ten")
-  end
-  
-  it "176514321007310 is one hundred and seventy six trillion five hundred and fourteen billion, three hundred and twenty one million seven thousand three hundred and ten" do
-    176514321007310.humanize.should eql("one hundred and seventy six trillion, five hundred and fourteen billion, three hundred and twenty one million, seven thousand three hundred and ten")
-  end
-  
-  it "2176514321007310 is two quadrillion, one hundred and seventy six trillion five hundred and fourteen billion, three hundred and twenty one million seven thousand three hundred and ten" do
-    2176514321007310.humanize.should eql("two quadrillion, one hundred and seventy six trillion, five hundred and fourteen billion, three hundred and twenty one million, seven thousand three hundred and ten")
-  end
-  
-  it "12176514321007310 is twelve quadrillion, one hundred and seventy six trillion five hundred and fourteen billion, three hundred and twenty one million seven thousand three hundred and ten" do
-    12176514321007310.humanize.should eql("twelve quadrillion, one hundred and seventy six trillion, five hundred and fourteen billion, three hundred and twenty one million, seven thousand three hundred and ten")
-  end
+
   
   
 end
