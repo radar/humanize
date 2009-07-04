a6a52731962f2e73db7b38489cb113b435fdde3a
Ryan Bigg <radarlistener@gmail.com>
2009-07-04 18:14:19 +1000

    Fix specs.

--- lib/humanize.rb
+++ lib/humanize.rb
@@ -4,28 +4,30 @@ module Humanize
   STRANGE = %w(eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen)
   ONES = %w(zero one two three four five six seven eight nine ten)
   
-  def parse(num)
+  def humanize(with_hypen=false)
+    o = parse(self, with_hypen)
+    o = "negative " << o if self < 0
+    o << decimal_parse if self.is_a?(Float)
+    o
+  end
+  
+  def parse(num, with_hyphen)
+    num = num.abs
     case num
     when 1..10
-      ONES[self]
+      ONES[num]
     when 11..19
       STRANGE[self-11]
     when 20..99
       remainder = self % 10
       [TENS[(self / 10) - 2].to_s, remainder.humanize].join(" ")
-    
-    # I feel as though after this point I am repeating myself.
-    # Need to find way to find what the base is for the number.
-    when 100..999
-      base = 100
+    when 100..999999999999999999999999999999
+      log = Math.log10(num).floor
+      o_remainder = log % 3
+      log = (log - o_remainder) if o_remainder != 0 && log > 3
+      base = "1#{'0' * log}".to_i
       num.extra(base)
-    when 1000..999999
-      base = 1000
-      num.extra(base)
-    when 1000000..999999999
-      base = 1000000
-      num.extra(base)
-    end
+    end 
   end
   
   def parts_of(num)
@@ -40,14 +42,28 @@ module Humanize
     else
       0
     end
-    number = [self.parts_of(base).humanize, LOTS[lots]]
-    number += ["and", remainder.humanize] if remainder != 0
-    number.join(" ")
+    number = [self.parts_of(base).humanize]
+    number << " "
+    number << LOTS[lots]
+    if remainder != 0
+      if %w(hundred).include?(number.last)
+        number << " and " 
+      elsif /illion/.match(number.last)
+        number << ", "
+      else
+        number << " "
+      end
+      number << remainder.humanize
+    end
+    number.join
   end
   
-  def humanize
-    parse(self)
+  # Call this!
+  def decimal_parse
+    " point " << (self - self.floor).to_s.split(".").last.split("").map { |d| ONES[d.to_i] }.join(" ")
   end
+  
+
 end
 
 class Numeric
