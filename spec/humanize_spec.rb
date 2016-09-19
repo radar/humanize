fc3df1b9a27c469eb0f01d7475988f2237eb67ac
Andy Brody <git@abrody.com>
2016-09-18 23:52:39 -0400

    Switch rspec to new syntax and remove whitespace.

--- spec/humanize_spec.rb
+++ spec/humanize_spec.rb
@@ -4,13 +4,10 @@ require 'rspec'
 
 describe "Humanize" do
   require_relative 'tests'
-  
+
   TESTS.each do |num, human|
     it "#{num} is #{human}" do
-      num.humanize.should eql(human)
+      expect(num.humanize).to eql(human)
     end
-  end 
+  end
 end
-  
-  
-  
