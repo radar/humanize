939e279111bbdc3f38190a870fd4a02743bb4a40
Jérémy FRERE <frere.jeremy@gmail.com>
2016-10-27 11:42:51 +0200

    Adds I18n support (inc. fr locale) along with a configuration system

---
+++ spec/config_spec.rb
@@ -0,0 +1,35 @@
+require 'rubygems'
+require 'humanize'
+require 'rspec'
+
+module Humanize
+
+  describe Config do
+
+    after(:each) do
+      Humanize.reset_config
+    end
+
+    describe "#default_locale" do
+
+      it "default value is :en" do
+        expect(Humanize.config.default_locale).to eq(:en)
+      end
+
+      it "value can be changed using a block" do
+        Humanize.configure do |config|
+          config.default_locale = :fr
+        end
+        expect(Humanize.config.default_locale).to eq(:fr)
+      end
+
+      it "value can be changed directly" do
+        Humanize.config.default_locale = :fr
+        expect(Humanize.config.default_locale).to eq(:fr)
+      end
+
+    end
+
+  end
+
+end
